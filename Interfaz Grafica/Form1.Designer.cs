ca3fea5ec19b1cb1c0539a8528844963eb30e78c
EricC <EricC@ERIC>
2025-02-09 00:15:40 -0800

    Agregar archivos de proyecto.

---
+++ Interfaz Grafica/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Interfaz_Grafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem1 = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1 = new MenuStrip();
+            panel1 = new Panel();
+            listBox1 = new ListBox();
+            menuStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(0, 43);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 45);
+            label1.TabIndex = 0;
+            label1.Text = "CONTACTO";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(100, 123);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(316, 28);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(100, 173);
+            textBox2.Margin = new Padding(4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(316, 28);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(100, 224);
+            textBox3.Margin = new Padding(4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(316, 28);
+            textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Navy;
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(4, 270);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 37);
+            button1.TabIndex = 7;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Image = Properties.Resources.compartimiento;
+            button2.Location = new Point(404, 379);
+            button2.Name = "button2";
+            button2.Size = new Size(64, 64);
+            button2.TabIndex = 8;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Navy;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(119, 270);
+            button3.Name = "button3";
+            button3.Size = new Size(103, 37);
+            button3.TabIndex = 9;
+            button3.Text = "Vaciar";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(4, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 22);
+            label2.TabIndex = 10;
+            label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(4, 173);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 22);
+            label3.TabIndex = 11;
+            label3.Text = "Telefono:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(4, 225);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 22);
+            label4.TabIndex = 12;
+            label4.Text = "Correo:";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { acercaDeToolStripMenuItem1, salirToolStripMenuItem });
+            acercaDeToolStripMenuItem.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            acercaDeToolStripMenuItem.ForeColor = SystemColors.ControlLight;
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(71, 25);
+            acercaDeToolStripMenuItem.Text = "MENU";
+            acercaDeToolStripMenuItem.Click += acercaDeToolStripMenuItem_Click;
+            // 
+            // acercaDeToolStripMenuItem1
+            // 
+            acercaDeToolStripMenuItem1.Name = "acercaDeToolStripMenuItem1";
+            acercaDeToolStripMenuItem1.Size = new Size(154, 26);
+            acercaDeToolStripMenuItem1.Text = "Acerca de";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(154, 26);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.Navy;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { acercaDeToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(8, 3, 0, 3);
+            menuStrip1.Size = new Size(822, 31);
+            menuStrip1.TabIndex = 6;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(listBox1);
+            panel1.Location = new Point(471, 43);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(351, 400);
+            panel1.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(3, 4);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(349, 404);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(822, 444);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
+        private ToolStripMenuItem acercaDeToolStripMenuItem1;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel panel1;
+        private ListBox listBox1;
+    }
+}
---
+++ Interfaz Grafica/Form1.cs
@@ -0,0 +1,83 @@
+using System.Reflection.Emit;
+
+namespace Interfaz_Grafica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text.Trim();
+            string telefono = textBox2.Text.Trim();
+            string correo = textBox3.Text.Trim();
+
+
+            if (!string.IsNullOrWhiteSpace(nombre) && !string.IsNullOrWhiteSpace(telefono) && !string.IsNullOrWhiteSpace(correo))
+            {
+                MessageBox.Show("Se ha añadido a la lista!", "Envío Correcto");
+                listBox1.Items.Add($"{nombre} - {telefono} - {correo}");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor, completa todos los campos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un contacto para eliminar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
