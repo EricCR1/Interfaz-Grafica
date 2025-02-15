3b1a62be99388568e61972d285a3a0a237e582b5
EricC <al23760307@ite.edu.mx>
2025-02-14 19:49:52 -0800

    Interfaz grafica dinamica, funcionalidad completa

--- GUI_Controles/Form1.Designer.cs
+++ GUI_Controles/Form1.Designer.cs
@@ -29,23 +29,67 @@
         private void InitializeComponent()
         {
             this.userControl11 = new Control_Imagen.UserControl1();
+            this.userControl12 = new Control_Imagen.UserControl1();
+            this.userControl13 = new Control_Imagen.UserControl1();
+            this.userControl14 = new Control_Imagen.UserControl1();
+            this.userControl15 = new Control_Imagen.UserControl1();
+            this.userControl16 = new Control_Imagen.UserControl1();
             this.SuspendLayout();
             // 
             // userControl11
             // 
             this.userControl11.Location = new System.Drawing.Point(2, -3);
             this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(458, 334);
+            this.userControl11.Size = new System.Drawing.Size(218, 228);
             this.userControl11.TabIndex = 0;
+            // 
+            // userControl12
+            // 
+            this.userControl12.Location = new System.Drawing.Point(226, -3);
+            this.userControl12.Name = "userControl12";
+            this.userControl12.Size = new System.Drawing.Size(218, 228);
+            this.userControl12.TabIndex = 1;
+            // 
+            // userControl13
+            // 
+            this.userControl13.Location = new System.Drawing.Point(450, -3);
+            this.userControl13.Name = "userControl13";
+            this.userControl13.Size = new System.Drawing.Size(218, 228);
+            this.userControl13.TabIndex = 2;
+            // 
+            // userControl14
+            // 
+            this.userControl14.Location = new System.Drawing.Point(2, 231);
+            this.userControl14.Name = "userControl14";
+            this.userControl14.Size = new System.Drawing.Size(218, 228);
+            this.userControl14.TabIndex = 3;
+            // 
+            // userControl15
+            // 
+            this.userControl15.Location = new System.Drawing.Point(226, 231);
+            this.userControl15.Name = "userControl15";
+            this.userControl15.Size = new System.Drawing.Size(218, 228);
+            this.userControl15.TabIndex = 4;
+            // 
+            // userControl16
+            // 
+            this.userControl16.Location = new System.Drawing.Point(450, 231);
+            this.userControl16.Name = "userControl16";
+            this.userControl16.Size = new System.Drawing.Size(218, 228);
+            this.userControl16.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(675, 466);
+            this.Controls.Add(this.userControl16);
+            this.Controls.Add(this.userControl15);
+            this.Controls.Add(this.userControl14);
+            this.Controls.Add(this.userControl13);
+            this.Controls.Add(this.userControl12);
             this.Controls.Add(this.userControl11);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
@@ -53,6 +97,11 @@
         #endregion
 
         private Control_Imagen.UserControl1 userControl11;
+        private Control_Imagen.UserControl1 userControl12;
+        private Control_Imagen.UserControl1 userControl13;
+        private Control_Imagen.UserControl1 userControl14;
+        private Control_Imagen.UserControl1 userControl15;
+        private Control_Imagen.UserControl1 userControl16;
     }
 }
 
---
+++ VisorDeImagenes/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace VisorDeImagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(707, 387);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Navy;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(204, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecciona tu carpeta";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(234, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 194);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(507, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Muestra tus imagenes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 387);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
